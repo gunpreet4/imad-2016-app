3efa50f42ed4b5edaeb52605c4806c886ad776f5
gunpreet4 <bhatiagunpreet@gmail.com>
2016-09-24 14:08:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function (req,res){
     var articleName=req.params.articleName;
-     res.send(createtemplate(articles[articeName]));
+     res.send(createtemplate(articles[articleName]));
 });
 
 
