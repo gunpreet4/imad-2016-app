a30e506527ba733d677c630277f35c4c83ff857b
gunpreet4 <bhatiagunpreet@gmail.com>
2016-09-24 16:32:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML= 'New Value';
+var image=document.getElementById('madi');
+image.onclick=function(){
+    image.style.marginLeft='100px';
+}
